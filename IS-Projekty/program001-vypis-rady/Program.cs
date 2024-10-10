a50ffc10cdd197910f9350c5e0281c0e66557330
Veronika Jiraskova <veronika.jiraskova@tul.cz>
2024-10-10 13:31:43 +0200

    #1 základní program s možností opakování

---
+++ IS-Projekty/program001-vypis-rady/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Specialized;
+
+class Program {
+
+    static void Main() {
+
+        //chci, aby se program opakoval po stisku klávesy a
+        string again = "a"; //= je přiřazení hodnoty, vyhodnocuje se zprava doleva
+        while(again == "a") {
+            Console.Clear();
+            Console.WriteLine("****************************");
+            Console.WriteLine("*******Název programu*******");
+            Console.WriteLine("****************************");
+            Console.WriteLine("*****Veronika Jirásková*****");
+            Console.WriteLine("****************************\n\n");
+            Console.WriteLine();
+
+            //vstup od uživatele - TO DO
+
+            //logika pro výpis řady - TO DO
+
+            //opakování programu - TO DO 
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+
+
+
+        }
+
+    }
+
+}
