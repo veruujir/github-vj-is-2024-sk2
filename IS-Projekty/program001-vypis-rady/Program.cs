70a79c8a9a762d5bf7f3941b0658667fd586ec08
Veronika Jiraskova <veronika.jiraskova@tul.cz>
2024-10-10 13:54:05 +0200

    #1 vstup hodnot a jejeich výpis

--- IS-Projekty/program001-vypis-rady/Program.cs
+++ IS-Projekty/program001-vypis-rady/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Specialized;
+using System.Threading.Tasks.Dataflow;
 
 class Program {
 
@@ -16,7 +17,37 @@ class Program {
             Console.WriteLine("****************************\n\n");
             Console.WriteLine();
 
-            //vstup od uživatele - TO DO
+            //vstup od uživatele - špatná varianta TO DO
+            //Console.Write("Zadejte první číslo řady: ");
+            //int first = int.Parse(Console.ReadLine());
+
+            //vstup od uživatele - lepší varianta TO DO
+            Console.Write("Zadejte první číslo řady (celé číslo): ");
+            int first;
+            while(!int.TryParse(Console.ReadLine(), out first)){
+                Console.WriteLine("Nezadali jste celé číslo. Zadejte znovu první číslo řady:");
+            }
+
+            Console.Write("Zadejte poslední číslo řady (celé číslo): ");
+            int last;
+            while(!int.TryParse(Console.ReadLine(), out last)){
+                Console.WriteLine("Nezadali jste celé číslo. Zadejte znovu poslední číslo řady:");
+            }
+
+            Console.Write("Zadejte diferenci: ");
+            int step;
+            while(!int.TryParse(Console.ReadLine(), out step)){
+                Console.WriteLine("Nezadali jste celé číslo. Zadejte znovu diferenci:");
+
+            //výpis uživatelského vstupu
+            Console.WriteLine();
+            Console.WriteLine("================================================");
+            Console.WriteLine("První číslo řady: {0}", first);
+            Console.WriteLine("Poslední číslo řady: {0},", last);
+            Console.WriteLine("Diference řady: {0}", step);
+            Console.WriteLine();
+            Console.WriteLine();
+
 
             //logika pro výpis řady - TO DO
 
@@ -30,4 +61,4 @@ class Program {
 
     }
 
-}
+}}
