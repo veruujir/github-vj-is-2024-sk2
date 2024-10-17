58e2ddcad9dc805aba8fc89a57f376858e475459
Veronika Jiraskova <veronika.jiraskova@tul.cz>
2024-10-17 13:23:49 +0200

    #2 konec

---
+++ IS-Projekty/Program000a-zakladni-kod/zakladni-kod.cs
@@ -0,0 +1,36 @@
+
+class Program {
+
+    static void Main() {
+
+        //chci, aby se program opakoval po stisku klávesy a
+        string again = "a"; //= je přiřazení hodnoty, vyhodnocuje se zprava doleva
+        while(again == "a") {
+            Console.Clear();
+            Console.WriteLine("****************************");
+            Console.WriteLine("*******Název programu*******");
+            Console.WriteLine("****************************");
+            Console.WriteLine("*****Veronika Jirásková*****");
+            Console.WriteLine("****************************\n\n");
+            Console.WriteLine();
+
+
+            //vstup od uživatele - lepší varianta TO DO
+            Console.Write("Zadejte první číslo řady (celé číslo): ");
+            int first;
+            while(!int.TryParse(Console.ReadLine(), out first)){
+                Console.WriteLine("Nezadali jste celé číslo. Zadejte znovu první číslo řady:");
+            }
+            }
+
+            //opakování programu - TO DO 
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+
+
+
+        }
+
+    }
+
+}
---
+++ IS-Projekty/program002ok-soucet-cifer/Program.cs
@@ -0,0 +1,49 @@
+﻿//chci, aby se program opakoval po stisku klávesy a
+        string again = "a"; //= je přiřazení hodnoty, vyhodnocuje se zprava doleva
+        while(again == "a") {
+            Console.Clear();
+            Console.WriteLine("***********************************");
+            Console.WriteLine("*******Součet a součin cifer*******");
+            Console.WriteLine("***********************************");
+            Console.WriteLine("********Veronika Jirásková*********");
+            Console.WriteLine("***********************************\n\n");
+            Console.WriteLine();
+
+
+            //vstup od uživatele - lepší varianta TO DO
+            Console.Write("Zadejte celé číslo řady: ");
+            int number;
+            while(!int.TryParse(Console.ReadLine(), out number)){
+                Console.WriteLine("Nezadali jste celé číslo. Zadejte znovu první číslo řady:");
+            }
+             //výpis vstupní hodnoty
+             Console.WriteLine("");
+             Console.WriteLine("Uživatel zadal: {0}", number);
+             Console.WriteLine("");
+
+            int suma = 0;
+            int numberBackup = number;
+            int digit;
+
+            if(number < 0){
+                number = - number;
+            }
+
+            while(number >= 10) {
+                digit = number % 10; //operátor modulo (určení zbytku po dělení číslem)
+                number = (number - digit) / 10;
+                Console.WriteLine("Digit = {0}", digit);
+                suma = suma + digit;
+            }
+            Console.WriteLine("Digit = {0}", number);
+            suma = suma + number;
+
+            Console.WriteLine("\n\nSoučet cifer čísla {0} je {1}", numberBackup, suma);
+
+
+
+
+            //opakování programu 
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+        }
