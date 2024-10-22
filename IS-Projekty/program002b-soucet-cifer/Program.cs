97dec3efd382a476f8518e55ad63e3c4e2b81e1c
Veronika Jiraskova <veronika.jiraskova@tul.cz>
2024-10-22 17:45:23 +0200

    #2 součet jinak

---
+++ IS-Projekty/program002b-soucet-cifer/Program.cs
@@ -0,0 +1,46 @@
+﻿  // Opakování programu po stisknutí klávesy 'a'
+        string again = "a";
+        while (again == "a")
+        {
+            Console.Clear();
+            Console.WriteLine("***********************************");
+            Console.WriteLine("*******Součet cifer*******");
+            Console.WriteLine("***********************************");
+            Console.WriteLine("********Veronika Jirásková*********");
+            Console.WriteLine("***********************************\n\n");
+
+            // Vstup od uživatele
+            Console.Write("Zadejte celé číslo řady: ");
+            int number;
+            while (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("Nezadali jste celé číslo. Zadejte znovu první číslo řady:");
+            }
+
+            // Výpis vstupní hodnoty
+            Console.WriteLine("");
+            Console.WriteLine("Uživatel zadal: {0}", number);
+            Console.WriteLine("");
+
+            int suma = 0;
+            int numberBackup = number;
+
+            // Pokud je číslo záporné, pracujeme s jeho absolutní hodnotou
+            if (number < 0)
+            {
+                number = -number;
+            }
+
+            // Součet cifer pomocí odečtu '0' od ASCII hodnoty
+            foreach (char digitChar in number.ToString())
+            {
+                int digit = digitChar - '0'; // Získání hodnoty číslice
+                suma += digit;
+            }
+
+            Console.WriteLine("\n\nSoučet cifer čísla {0} je {1}", numberBackup, suma);
+
+            // Opakování programu
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+        }
