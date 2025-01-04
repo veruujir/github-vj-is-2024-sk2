5dda07aee4181fc4753eeac2c0dbf4bda022f5a5
Veronika Jiraskova <veronika.jiraskova@tul.cz>
2025-01-04 16:04:15 +0100

    #20 idk

--- IS-Projekty/priprava-na-zapocet/obj/Debug/net7.0/priprava-na-zapocet.AssemblyInfo.cs
+++ IS-Projekty/priprava-na-zapocet/obj/Debug/net7.0/priprava-na-zapocet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("priprava-na-zapocet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fd82bf83895169c52ad273cbcd4d84bec39fd58")]
 [assembly: System.Reflection.AssemblyProductAttribute("priprava-na-zapocet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("priprava-na-zapocet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
